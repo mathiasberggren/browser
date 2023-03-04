c499b9fce1afb6268fdad3c0d23d9cf13bf38f5d
Mathias Berggren <mathias.berggren@klarna.com>
2023-03-04 21:12:30 +0100

    Render a window

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
-// use html5ever::tendril::TendrilSink;
-// use std::env;
-// use url::Url;
+use vulkano::VulkanLibrary;
+use vulkano::{instance::Instance, instance::InstanceCreateInfo};
+use winit::event_loop::{EventLoop};
+use winit::window::{WindowBuilder};
 
 #[tokio::main]
-// TODO (alivenotions): Add a custom error here
 async fn main() -> Result<(), reqwest::Error> {
-    Ok(())
+
+let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
+let instance = Instance::new(library, InstanceCreateInfo {
+    enumerate_portability: true,
+    ..Default::default()
+}).expect("failed to create instance");
+
+for physical_device in instance.enumerate_physical_devices().unwrap() {
+    println!("Available device: {}", physical_device.properties().device_name);
+};
+
+
+let event_loop = EventLoop::new();  // ignore this for now
+
+// The surface is a cross-platform abstraction over the actual window object, that Vulkano can use for rendering
+let _surface = WindowBuilder::new()
+    .build(&event_loop)
+    .unwrap();
+
+    use winit::event::{Event, WindowEvent};
+    use winit::event_loop::ControlFlow;
+        
+    // Create infinity loop
+    event_loop.run(|event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            },
+            _ => ()
+        }
+    });
+
 }
 
